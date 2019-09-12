afe85ae11a60292551361882ab0971cd027d1e8f
emerson <emerson.vv75248@gmail.com>
2019-09-12 12:38:42 -0300

    oi

---
+++ schedule.h
@@ -0,0 +1,33 @@
+#ifndef SCHEDULE_H
+#define SCHEDULE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned long int ULint;
+
+typedef struct schedule{
+  ULint id;
+  char name[100];
+  int age;
+  int cep;
+} Schedule;
+
+void * pBuffer;
+
+ULint * init();
+
+void put();
+void pop();
+void search();
+void list();
+
+void menu(ULint * op);
+
+Schedule * allocSchedule();
+void destroySchedule(ULint * ids);
+
+ULint bufferLength();
+
+#endif /* SCHEDULE_H */
